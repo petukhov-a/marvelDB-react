cd698cf356c01935bae86a5142fdaee562536559
Anton <reopik@mail.ru>
2023-07-31 20:45:34 +0300

    made routing between pages Main and Comics

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+import MainPage from "./MainPage";
+import ComicsPage from "../comicsList/ComicsList";
+
+export {MainPage, ComicsPage};
--- src/index.js
+++ src/index.js
@@ -6,7 +6,5 @@ import './style/style.scss';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   );
